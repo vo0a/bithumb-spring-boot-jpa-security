578916941a86016a7488fc7c2d87008225f4b095
daromKim <vo0a195@gmail.com>
2021-09-09 22:45:35 +0900

    feat: jwt token provider

---
+++ src/main/java/com/romkudev/api/security/domain/UserDetailsServiceImpl.java
@@ -0,0 +1,9 @@
+package com.romkudev.api.security.domain;
+
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class UserDetailsServiceImpl {
+    public UserDetails loadUserByUsername(String username) {
+        return null;
+    }
+}
--- src/main/java/com/romkudev/api/user/domain/User.java
+++ src/main/java/com/romkudev/api/user/domain/User.java
@@ -4,6 +4,7 @@ import com.romkudev.api.order.domain.Order;
 import lombok.Data;
 
 import javax.persistence.*;
+import javax.validation.constraints.Size;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,16 +17,17 @@ public class User {
     @Column(name = "user_id")
     private Long userId;
 
-    @Column(name = "username")
+    @Column(name = "username", unique = true, nullable = false)
     private String username;
 
     @Column(name = "password")
+    @Size(min = 8, message = "8자리 이상 입력하세요")
     private String password;
 
-    @Column(name = "name")
+    @Column(name = "name", nullable = false)
     private String name;
 
-    @Column(name = "email")
+    @Column(name = "email", unique = true, nullable = false)
     private String email;
 
     @Column(name = "reg_date")
@@ -33,4 +35,7 @@ public class User {
 
     @OneToMany(mappedBy = "user")
     private List<Order> orders = new ArrayList<>();
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    List<Role> roles;
 }
