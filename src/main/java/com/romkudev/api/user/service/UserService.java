8043b8491edb93ca8df05760a544b2129795c610
daromKim <vo0a195@gmail.com>
2021-09-13 23:49:09 +0900

    feat: 사용자 회원가입/로그인

--- src/main/java/com/romkudev/api/user/service/UserService.java
+++ src/main/java/com/romkudev/api/user/service/UserService.java
@@ -1,6 +1,7 @@
 package com.romkudev.api.user.service;
 
 import com.romkudev.api.user.domain.User;
+import com.romkudev.api.user.domain.UserDto;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -20,4 +21,8 @@ public interface UserService {
     long count();
 
     void deleteById(Long id);
+
+    String signup(User user);
+
+    UserDto signin(User user);
 }
--- src/main/java/com/romkudev/api/user/service/UserServiceImpl.java
+++ src/main/java/com/romkudev/api/user/service/UserServiceImpl.java
@@ -1,19 +1,32 @@
 package com.romkudev.api.user.service;
 
+import com.romkudev.api.security.domain.SecurityProvider;
+import com.romkudev.api.security.exception.SecurityRuntimeException;
+import com.romkudev.api.user.domain.Role;
 import com.romkudev.api.user.domain.User;
+import com.romkudev.api.user.domain.UserDto;
 import com.romkudev.api.user.repository.UserRepository;
 import com.romkudev.api.util.Proxy;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.modelmapper.ModelMapper;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
+@Slf4j
 public class UserServiceImpl extends Proxy implements UserService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final SecurityProvider provider;
+    private final ModelMapper modelMapper;
 
     @Override
     public List<User> findAll() {
@@ -44,5 +57,37 @@ public class UserServiceImpl extends Proxy implements UserService {
     @Override
     public void deleteById(Long id) {
         userRepository.deleteById(id);
+    }
+
+    @Override
+    public String signup(User user) {
+        if (!userRepository.existsByUsername(user.getUsername())) {
+            user.setPassword(passwordEncoder.encode(user.getPassword()));
+            List<Role> list = new ArrayList<>();
+            list.add(Role.USER);
+            user.setRoles(list);
+            userRepository.save(user);
+            log.info("user" + user);
+            return provider.createToken(user.getUsername(), user.getRoles());
+        } else {
+            throw new SecurityRuntimeException("중복된 ID 입니다", HttpStatus.UNPROCESSABLE_ENTITY);
+        }
+    }
+
+    @Override
+    public UserDto signin(User user) {
+        try {
+            UserDto userDto = modelMapper.map(user, UserDto.class);
+            String token = (passwordEncoder.matches(
+                    user.getPassword(),
+                    userRepository.findByUsername(user.getUsername()).get().getPassword()))
+                    ? provider.createToken(user.getUsername(), userRepository.findByUsername(user.getUsername()).get().getRoles())
+                    : "Wrong Password";
+            userDto.setToken(token);
+            return userDto;
+
+        } catch (Exception e) {
+            throw new SecurityRuntimeException("유효하지 않은 아이디 / 비밀번호", HttpStatus.UNPROCESSABLE_ENTITY);
+        }
     }
 }
