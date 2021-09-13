6d1098c35c81c55d9cb31508e9212535401ac546
daromKim <vo0a195@gmail.com>
2021-09-14 03:56:21 +0900

    feat: 사용자 회원가입/로그인

--- src/main/java/com/romkudev/api/security/domain/UserDetailsServiceImpl.java
+++ src/main/java/com/romkudev/api/security/domain/UserDetailsServiceImpl.java
@@ -1,9 +1,26 @@
 package com.romkudev.api.security.domain;
 
+import com.romkudev.api.user.domain.User;
+import com.romkudev.api.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
-public class UserDetailsServiceImpl {
-    public UserDetails loadUserByUsername(String username) {
-        return null;
+import javax.transaction.Transactional;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<User> user = Optional.ofNullable(userRepository.findByUsername(username))
+                .orElseThrow(() -> new UsernameNotFoundException("User Not Found with username : "+ username));
+        return UserDetailsImpl.build(user.get());
     }
 }
--- src/main/java/com/romkudev/api/user/controller/UserController.java
+++ src/main/java/com/romkudev/api/user/controller/UserController.java
@@ -1,14 +1,50 @@
 package com.romkudev.api.user.controller;
 
 import com.romkudev.api.user.domain.User;
+import com.romkudev.api.user.domain.UserDto;
+import com.romkudev.api.user.service.UserServiceImpl;
+import io.swagger.annotations.*;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
 
+@CrossOrigin(origins = "*", allowedHeaders = "false")
+@Api(tags = "users")
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/users")
 public class UserController {
+
+    private final UserServiceImpl userService;
+    private final ModelMapper modelMapper;
+
+    @PostMapping("/signup")
+    @ApiOperation(value = "${UserController.signup}")
+    @ApiResponses(value = {
+            @ApiResponse(code = 400, message = "something wrong"),
+            @ApiResponse(code = 403, message = "승인거절"),
+            @ApiResponse(code = 422, message = "중복된 username")})
+    public ResponseEntity<String> signup(@ApiParam("Signup User")
+                                         @RequestBody UserDto userDto) {
+        System.out.println("################## MMMMMMM ###########");
+        return ResponseEntity.ok(userService.signup(modelMapper.map(userDto, User.class)));
+    }
+
+    @PostMapping("/signin")
+    @ApiOperation(value = "${UserController.signin}")
+    @ApiResponses(value = {
+            @ApiResponse(code = 400, message = "something wrong"),
+            @ApiResponse(code = 422, message = "유효하지 않은 아이디 / 비밀번호")})
+    public ResponseEntity<UserDto> signin(@ApiParam("Signin User")
+                                          @RequestBody UserDto userDto) {
+        return ResponseEntity.ok(userService.signin(modelMapper.map(userDto, User.class)));
+    }
+
+
     @GetMapping()
     public List<User> findAll() {
         return null;
@@ -44,3 +80,5 @@ public class UserController {
 
     }
 }
+
+
